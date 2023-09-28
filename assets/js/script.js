b559247f109ee2c8d39f86d774aae780563fe7e4
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-09-28 12:36:07 +0200

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 import { ErrorDisplay, UserFeedback } from "./modules/errorMessagesSystem.js";
-const inputValue = document.getElementById('inputOne');
-//ErrorDisplay('alert', 'Din input skal have en værdi');
-UserFeedback('succes', `virker!`, 'userFeedback');
 
-// knap fungere ikke, fix
-/* function submitValue() {
+//ErrorDisplay('alert', 'Din input skal have en værdi');
+//UserFeedback('succes', `virker!`, 'userFeedback');
+
+
+
+
+const myButton = document.getElementById('submitValue');
+
+myButton.addEventListener('click', (e) => {
+    submitValue()
+})
+
+
+function submitValue() {
+    const inputValue = document.getElementById('inputOne');
     
     if(inputValue.value === '') {
         ErrorDisplay('alert', 'Din input skal have en værdi');
     } else {
         UserFeedback('succes', `${inputValue.value}`, 'userFeedback');
     }
-} */
+}
